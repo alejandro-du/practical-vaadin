8b52c8a93d25638b7d0301595c3983b1be095b4b
Alejandro Duarte <alejandro@vaadin.com>
2021-03-18 18:25:37 +0200

    Updates to Vaadin 19.0.1

--- ch05/binder/pnpmfile.js
+++ ch05/binder/pnpmfile.js
@@ -8,37 +8,35 @@
 
 const fs = require('fs');
 
-const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
-
 const versionsFile = require('path').resolve(__dirname, 'target/frontend/versions.json');
 
 if (!fs.existsSync(versionsFile)) {
-    return;
+  return;
 }
 const versions = JSON.parse(fs.readFileSync(versionsFile, 'utf-8'));
 
 module.exports = {
-    hooks: {
-        readPackage
-    }
+  hooks: {
+    readPackage
+  }
 };
 
 function readPackage(pkg) {
-    const {dependencies} = pkg;
-
-    if (dependencies) {
-        for (let k in versions) {
-            if (dependencies[k] && dependencies[k] !== versions[k]) {
-                pkg.dependencies[k] = versions[k];
-            }
-        }
+  const {dependencies} = pkg;
+  
+  if (dependencies) {
+    for (let k in versions) {
+      if (dependencies[k] && dependencies[k] !== versions[k]) {
+        pkg.dependencies[k] = versions[k];
+      }
     }
+  }
 
-    // Forcing chokidar version for now until new babel version is available
-    // check out https://github.com/babel/babel/issues/11488
-    if (pkg.dependencies.chokidar) {
-        pkg.dependencies.chokidar = '^3.4.0';
-    }
+  // Forcing chokidar version for now until new babel version is available
+  // check out https://github.com/babel/babel/issues/11488
+  if (pkg.dependencies.chokidar) {
+    pkg.dependencies.chokidar = '^3.4.0';
+  }
 
-    return pkg;
+  return pkg;
 }
