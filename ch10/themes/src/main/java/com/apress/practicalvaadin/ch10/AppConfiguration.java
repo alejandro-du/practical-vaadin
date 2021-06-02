804704d5004ecd31fdff587ca7f71b954b036629
Alejandro Duarte <alejandro@vaadin.com>
2021-06-02 12:35:18 +0300

    Set Material Theme the default in chapter 10"

--- ch10/themes/src/main/java/com/apress/practicalvaadin/ch10/AppConfiguration.java
+++ ch10/themes/src/main/java/com/apress/practicalvaadin/ch10/AppConfiguration.java
@@ -5,8 +5,8 @@ import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
 import com.vaadin.flow.theme.material.Material;
 
-//@Theme(themeClass = Material.class)
-@Theme(themeClass = Lumo.class, variant = Lumo.DARK)
+@Theme(themeClass = Material.class)
+//@Theme(themeClass = Lumo.class, variant = Lumo.DARK)
 public class AppConfiguration implements AppShellConfigurator {
 
 }
